1489915f974f77544dd6aafe679bd4ae3aa7fc3f
Mark Brody <markbrody@users.noreply.github.com>
2020-05-05 22:33:27 -0500

    add a trailing line as test

--- routes/web.php
+++ routes/web.php
@@ -51,3 +51,4 @@ Route::group(["prefix" => "images"], function() {
 Route::get("login", "Auth\LoginController@showLoginForm")->name("login");
 Route::post("login", "Auth\LoginController@login");
 Route::get("logout", "Auth\LoginController@logout")->name("logout");
+
